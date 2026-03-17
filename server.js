b8cc0a28f50ad6a42a1a4f9e93dd5356b7372a9a
a01067471329-png <a01067471329@gmail.com>
2026-03-17 10:23:52 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// 서버와 DB가 통신하는 방법
+let db;
+const url =
+  "mongodb+srv://a01067471329_db_user:Z3iSfVDAEZ7pGSK5@cluster0.t759otx.mongodb.net/?appName=Cluster0";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB 연결성공");
+    db = client.db("forum");
+
+    app.listen(3000, () => {
+      console.log("http://localhost:3000 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// 누군가가 메인페이지를 요청했을 때 "hello world" 라는 문자열로 응답해준다.
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/shop", (request, response) => {
+  db.collection("post").insertOne(
+    { name: "홍길동", age: 20, _id: 100 },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+  response.sendFile(__dirname + "/shop.html");
+});
+
+// async: 비동기처리 함수로 만들어준다. DB에서 데이터를 가져오는 작업이 끝날 때까지 기다려야 하기 때문에 async 함수를 사용한다.
+app.get("/list", async (request, response) => {
+  // await: 다음 줄을 처리하기 전에 기다린다. DB에서 데이터를 가져오는 작업이 끝날 때까지 기다린다.
+  let db_result = await db
+    .collection("post")
+    .find()
+    .toArray((err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  // db_result는 array 형태로 DB에서 가져온 데이터를 담고 있다.
+  console.log(db_result);
+  response.render("list", { posts: db_result });
+});
