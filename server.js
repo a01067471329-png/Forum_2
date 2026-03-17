f44e109ec4502d9a3470dbfe33bf4831267cef70
a01067471329-png <a01067471329@gmail.com>
2026-03-17 12:03:35 +0000

    update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 // 서버와 DB가 통신하는 방법
 let db;
@@ -29,15 +30,52 @@ app.get("/", (request, response) => {
 });
 
 app.get("/shop", (request, response) => {
-  db.collection("post").insertOne(
-    { name: "홍길동", age: 20, _id: 100 },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-    },
-  );
+  // db.collection("post").insertOne(
+  //   { name: "홍길동", age: 20, _id: 100 },
+  //   (err, result) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //   },
+  // );
   response.sendFile(__dirname + "/shop.html");
+
+  for (let i = 0; i < 3; i++) {
+    console.log(i);
+  }
+});
+
+app.get("/write", (request, response) => {
+  response.render("write");
+});
+
+app.post("/write", async (request, response) => {
+  try {
+    await db.collection("post").insertOne({
+      title: request.body.title,
+      content: request.body.content,
+    });
+    response.redirect("/list");
+  } catch (err) {
+    console.log(err);
+    response.status(500).send("Failed to save post.");
+  }
+});
+
+app.get("/detail/:id", async (request, response) => {
+  try {
+    const post = await db
+      .collection("post")
+      .findOne({ _id: new ObjectId(request.params.id) });
+
+    if (!post) return response.status(404).send("Post not found.");
+
+    response.render("detail", { post });
+  } catch (err) {
+    console.log(err);
+
+    response.status(500).send("Failed to load post.");
+  }
 });
 
 // async: 비동기처리 함수로 만들어준다. DB에서 데이터를 가져오는 작업이 끝날 때까지 기다려야 하기 때문에 async 함수를 사용한다.
@@ -52,9 +90,6 @@ app.get("/list", async (request, response) => {
       }
     });
   // db_result는 array 형태로 DB에서 가져온 데이터를 담고 있다.
-  console.log(db_result);
+  // console.log(db_result);
   response.render("list", { posts: db_result });
-
-  // new comment
-  // new!!
 });
